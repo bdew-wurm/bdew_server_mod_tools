3f741587ddc9b01d101d0c8f00918e275116a663
bdew <blackdew@gmail.com>
2019-04-09 10:08:45 +0300

    1.0.0 release

--- src/main/java/net/bdew/wurm/tools/server/internal/ModTools.java
+++ src/main/java/net/bdew/wurm/tools/server/internal/ModTools.java
@@ -12,7 +12,7 @@ import org.gotti.wurmunlimited.modloader.interfaces.ServerPollListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
 public class ModTools implements WurmServerMod, PreInitable, ServerPollListener {
-    public static final String VERSION = "0.9.0";
+    public static final String VERSION = "1.0.0";
 
     @Override
     public String getVersion() {
